8bb3c13519c980585a7d2f2a2d4c37af9c8e1816
Abel Pinales <abelpinales97@gmail.com>
2023-09-25 18:19:40 -0400

    swag

--- app/Liderazgo/page.js
+++ app/Liderazgo/page.js
@@ -3,7 +3,7 @@ import Header from "../component/headerComponent/header";
 import NewMember from "../component/newMemberComponent/newMember";
 import Footer from "../component/footerComponent/footer";
 import { EB_Garamond } from "next/font/google";
-import MainEyeCatch from "../component/liderazgoComponents/mainEyeCatchLiderazgo";
+import MainEyeCatch from "../component/ministeriosComponents/mainEyeCatchLiderazgo";
 
 // import SubEyeCatch from "../component/liderazgoComponents/subEyeCatch";
 // import MainEyeCatch from "../component/liderazgoComponents/mainEyeCatch";
