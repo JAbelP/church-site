e9344573d748a1a7e8684355e51a5360e7f4599d
JAbelP <abelpinales97@gmail.com>
2023-11-14 15:17:39 -0500

    "

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -6,9 +6,9 @@ export default function NotFound() {
       <Header />
       <div className="flex flex-col items-center justify-center flex-grow text-center text-3xl">
         <div className="w-3/5 mb-8">
-          "Trust in the Lord with all your heart and lean not on your own
+        &quot;Trust in the Lord with all your heart and lean not on your own
           understanding; in all your ways submit to him, and he will make your
-          paths straight." - Proverbs 3:5-6
+          paths straight.&quot; - Proverbs 3:5-6
         </div>
         <h1>404 - Page Not Found</h1>
       </div>
