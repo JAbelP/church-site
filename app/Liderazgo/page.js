0710f07e96af991a43e52caaded725f40f007660
JAbelP <abelpinales97@gmail.com>
2023-11-14 14:55:21 -0500

    SubEyeCatch has been updated

--- app/Liderazgo/page.js
+++ app/Liderazgo/page.js
@@ -10,33 +10,39 @@ import Image2 from "../../public/Leadership/Colon.jpg";
 import Image3 from "../../public/Leadership/Wellington.jpg";
 import Image4 from "../../public/Leadership/DeRosa.jpg";
 
-
 const ebG = EB_Garamond({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={ebG.className}>
       <div className="bg-white h-fit w-full flex flex-col text-black">
-      <div className="mt-28 mx-auto md:mt-0">
-        <NewMember />
-      </div>
-      <div className="mt-13 md:mt-0">
-        <Header />
-      </div>
+        <div className="mt-28 mx-auto md:mt-0">
+          <NewMember />
+        </div>
+        <div className="mt-13 md:mt-0">
+          <Header />
+        </div>
+        <p className="text-5xl font-medium tracking-widest mx-auto">PASTORES</p>
+        <div className="flex flex-col">
 
-        <SubEyeCatch
-          title={"PASTORES"}
-          names={"TOMAS Y INGRID RAMIREZ"}
-          imageLocation={Image1}
-          altText={"Pastores: TOMAS Y INGRID RAMIREZ"}
-        />
+          <div>
+            <SubEyeCatch
+              title={"PASTORES"}
+              names={"TOMAS Y INGRID RAMIREZ"}
+              imageLocation={Image1}
+              altText={"Pastores: TOMAS Y INGRID RAMIREZ"}
+            />
+          </div>
 
-        <MainEyeCatch
-          title={"PASTOR ASOCIADO"}
-          names={"ALDO Y BRENDA COLON"}
-          imageLocation={Image2}
-          altText={"PASTOR ASOCIADO: ALDO Y BRENDA COLON"}
-        />
+          <div>
+            <SubEyeCatch
+              title={"PASTOR ASOCIADO"}
+              names={"ALDO Y BRENDA COLON"}
+              imageLocation={Image2}
+              altText={"PASTOR ASOCIADO: ALDO Y BRENDA COLON"}
+            />
+          </div>
+        </div>
 
         <div className=" mt-16">
           <p className=" mb-8 text-center  text-4xl font-medium tracking-widest">
@@ -46,13 +52,13 @@ export default function Home() {
             <SubEyeCatch
               names={"WELLINGTON Y MARCIA DE JESUS"}
               imageLocation={Image3}
-              altText={"Example"}
+              altText={"Decon: WELLINGTON Y MARCIA DE JESUS"}
             />
 
             <SubEyeCatch
               names={"JOSE Y ANA PAULA DE LA ROSA"}
               imageLocation={Image4}
-              altText={"Example"}
+              altText={"Decon: JOSE Y ANA PAULA DE LA ROSA"}
             />
           </div>
         </div>
