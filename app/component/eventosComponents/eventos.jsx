88b31bd5a6206d805dec56301e0a2eedefa299b7
JAbelP <abelpinales97@gmail.com>
2023-12-06 16:16:12 -0500

    Forgot events

--- app/component/eventosComponents/eventos.jsx
+++ app/component/eventosComponents/eventos.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import localFont from "next/font/local";
 import Image from "next/image";
 import Text from "../textComponents/text";
+import LanguageSelector from "../flagComponents/flagSelector";
 const CopperplateBold = localFont({ src: "../../../font/CopperplateBold.ttf" });
 
 function Eventos({ eventName, eventTime, eventLocation, eventImage }) {
   debugger;
   return (
     <div className="flex flex-row">
+      <LanguageSelector />
       <div className="w-[502px] h-[224px] relative">
         <Image src={eventImage} alt="Example" fill={true} />
       </div>
